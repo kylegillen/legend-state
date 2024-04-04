9e7fdccd7dd836e31604a82e8a85f8418d9cf64b
kylegillen <kyle@nextriot.com>
2024-04-03 21:01:57 -0300

    migrate to react-query v5

--- src/persist-plugins/query.ts
+++ src/persist-plugins/query.ts
@@ -1,133 +1,119 @@
 import {
-    ObservablePersistRemoteFunctions,
-    isFunction,
-    observe,
-    type ObservablePersistRemoteSetParams,
+  ObservablePersistRemoteFunctions,
+  isFunction,
+  observe,
+  type ObservablePersistRemoteSetParams,
 } from '@legendapp/state';
 import {
-    InfiniteQueryObserver,
-    MutationObserver,
-    QueryClient,
-    QueryKey,
-    QueryObserver,
-    UseBaseQueryOptions,
-    UseMutationOptions,
-    notifyManager,
-    useQueryClient,
+  InfiniteQueryObserver,
+  MutationObserver,
+  QueryClient,
+  QueryKey,
+  QueryObserver,
+  UseBaseQueryOptions,
+  UseMutationOptions,
+  useQueryClient,
 } from '@tanstack/react-query';
 
 type ObservableQueryOptions<TQueryFnData, TError, TData, TQueryData, TQueryKey extends QueryKey> = Omit<
-    UseBaseQueryOptions<TQueryFnData, TError, TData, TQueryData, TQueryKey>,
-    'queryKey'
+  UseBaseQueryOptions<TQueryFnData, TError, TData, TQueryData, TQueryKey>,
+  'queryKey'
 > & { queryKey?: TQueryKey | (() => TQueryKey) };
 
 type Params<TQueryFnData, TError, TData, TQueryData, TQueryKey extends QueryKey> = {
-    query: ObservableQueryOptions<TQueryFnData, TError, TData, TQueryData, TQueryKey>;
-    mutation?: UseMutationOptions<TData, TError, void>;
-    type?: 'Query' | 'Infinite';
+  query: ObservableQueryOptions<TQueryFnData, TError, TData, TQueryData, TQueryKey>;
+  mutation?: UseMutationOptions<TData, TError, void>;
+  type?: 'Query' | 'Infinite';
 } & (
     | {
-          queryClient: QueryClient;
-          useContext?: false | undefined | never;
-      }
+      queryClient: QueryClient;
+      useContext?: false | undefined | never;
+    }
     | {
-          queryClient?: QueryClient;
-          useContext?: true;
-      }
-);
+      queryClient?: QueryClient;
+      useContext?: true;
+    }
+  );
 
 export function persistPluginQuery<TObs, TQueryFnData, TError, TData, TQueryData, TQueryKey extends QueryKey>({
-    query: options,
-    mutation: mutationOptions,
-    type = 'Query',
-    queryClient,
-    useContext,
+  query: options,
+  mutation: mutationOptions,
+  type = 'Query',
+  queryClient,
+  useContext,
 }: Params<TQueryFnData, TError, TData, TQueryData, TQueryKey>): ObservablePersistRemoteFunctions<TObs> {
-    if (useContext) {
-        queryClient = queryClient || useQueryClient();
-    }
-    // Set up the defaults like useBaseQuery does
-    const defaultedOptions = queryClient!.defaultQueryOptions(
-        options as UseBaseQueryOptions<TQueryFnData, TError, TData, TQueryData, TQueryKey>,
-    );
+  if (useContext) {
+    queryClient = queryClient || useQueryClient();
+  }
+  // Set up the defaults like useBaseQuery does
+  const defaultedOptions = queryClient!.defaultQueryOptions(
+    options as UseBaseQueryOptions<TQueryFnData, TError, TData, TQueryData, TQueryKey>,
+  );
 
-    // Include callbacks in batch renders
-    if (defaultedOptions.onError) {
-        defaultedOptions.onError = notifyManager.batchCalls(defaultedOptions.onError);
-    }
+  const Observer = type === 'Query' ? QueryObserver : (InfiniteQueryObserver as typeof QueryObserver);
 
-    if (defaultedOptions.onSuccess) {
-        defaultedOptions.onSuccess = notifyManager.batchCalls(defaultedOptions.onSuccess);
-    }
+  const ret: ObservablePersistRemoteFunctions<unknown> = {
+    get({ onChange }) {
+      let observer: QueryObserver<TQueryFnData, TError, TData, TQueryData, TQueryKey> | undefined = undefined;
+      let latestOptions = defaultedOptions;
+      let queryKeyFromFn: TQueryKey;
 
-    if (defaultedOptions.onSettled) {
-        defaultedOptions.onSettled = notifyManager.batchCalls(defaultedOptions.onSettled);
-    }
+      const origQueryKey = options.queryKey!;
 
-    const Observer = type === 'Query' ? QueryObserver : (InfiniteQueryObserver as typeof QueryObserver);
+      // If the queryKey is a function, observe it and extract the raw value
+      const isKeyFunction = isFunction(origQueryKey);
+      if (isKeyFunction) {
+        observe(({ num }) => {
+          queryKeyFromFn = origQueryKey();
+          if (num > 0) {
+            updateQueryOptions(latestOptions);
+          }
+        });
+      }
 
-    const ret: ObservablePersistRemoteFunctions<unknown> = {
-        get({ onChange }) {
-            let observer: QueryObserver<TQueryFnData, TError, TData, TQueryData, TQueryKey> | undefined = undefined;
-            let latestOptions = defaultedOptions;
-            let queryKeyFromFn: TQueryKey;
+      const updateQueryOptions = (obj: any) => {
+        // Since legend-state mutates the query options, we need to clone it to make Query
+        // see it as changed
+        const options = Object.assign({}, obj);
 
-            const origQueryKey = options.queryKey!;
+        // Use the latest value from the observed queryKey function
+        if (isKeyFunction) {
+          options.queryKey = queryKeyFromFn;
+        }
 
-            // If the queryKey is a function, observe it and extract the raw value
-            const isKeyFunction = isFunction(origQueryKey);
-            if (isKeyFunction) {
-                observe(({ num }) => {
-                    queryKeyFromFn = origQueryKey();
-                    if (num > 0) {
-                        updateQueryOptions(latestOptions);
-                    }
-                });
-            }
+        latestOptions = options;
 
-            const updateQueryOptions = (obj: any) => {
-                // Since legend-state mutates the query options, we need to clone it to make Query
-                // see it as changed
-                const options = Object.assign({}, obj);
+        // Update the Query options
+        if (observer) {
+          observer.setOptions(options, { listeners: false });
+        }
+      };
+      updateQueryOptions(defaultedOptions);
 
-                // Use the latest value from the observed queryKey function
-                if (isKeyFunction) {
-                    options.queryKey = queryKeyFromFn;
-                }
+      // Create the observer
+      observer = new Observer!<TQueryFnData, TError, TData, TQueryData, TQueryKey>(queryClient!, latestOptions);
 
-                latestOptions = options;
+      // Get the initial optimistic results if it's already cached
+      const result = observer!.getOptimisticResult(latestOptions);
+      // Subscribe to Query's observer and update the observable
+      observer!.subscribe((result) => {
+        onChange({ value: result.data });
+      });
 
-                // Update the Query options
-                if (observer) {
-                    observer.setOptions(options, { listeners: false });
-                }
-            };
-            updateQueryOptions(defaultedOptions);
+      // Return the initial data
+      if (result) {
+        return result.data;
+      }
+    },
+  };
 
-            // Create the observer
-            observer = new Observer!<TQueryFnData, TError, TData, TQueryData, TQueryKey>(queryClient!, latestOptions);
-
-            // Get the initial optimistic results if it's already cached
-            const result = observer!.getOptimisticResult(latestOptions);
-            // Subscribe to Query's observer and update the observable
-            observer!.subscribe((result) => {
-                onChange({ value: result.data });
-            });
-
-            // Return the initial data
-            if (result) {
-                return result.data;
-            }
-        },
+  if (mutationOptions) {
+    const mutator = new MutationObserver(queryClient!, mutationOptions);
+    // When the observable changes call the mutator function
+    ret.set = async ({ value }: ObservablePersistRemoteSetParams<any>) => {
+      mutator.mutate(value);
     };
+  }
 
-    if (mutationOptions) {
-        const mutator = new MutationObserver(queryClient!, mutationOptions);
-        // When the observable changes call the mutator function
-        ret.set = async ({ value }: ObservablePersistRemoteSetParams<any>) => {
-            mutator.mutate(value);
-        };
-    }
-
-    return ret as any;
+  return ret as any;
 }
--- src/react-hooks/useObservableQuery.ts
+++ src/react-hooks/useObservableQuery.ts
@@ -5,201 +5,187 @@
 
 import { observable, observe, type ObservableObject } from '@legendapp/state';
 import {
-    DefaultedQueryObserverOptions,
-    MutationObserver,
-    Query,
-    QueryClient,
-    QueryKey,
-    QueryObserver,
-    QueryObserverResult,
-    UseErrorBoundary,
-    notifyManager,
+  DefaultedQueryObserverOptions,
+  MutationObserver,
+  Query,
+  QueryClient,
+  QueryKey,
+  QueryObserver,
+  QueryObserverResult
 } from '@tanstack/query-core';
 import {
-    UseBaseQueryOptions,
-    UseMutationOptions,
-    useIsRestoring,
-    useQueryClient,
-    useQueryErrorResetBoundary,
-    type UseBaseQueryResult,
+  ThrowOnError,
+  UseBaseQueryOptions,
+  UseMutationOptions,
+  useIsRestoring,
+  useQueryClient,
+  useQueryErrorResetBoundary,
+  type UseBaseQueryResult,
 } from '@tanstack/react-query';
-import type { QueryErrorResetBoundaryValue } from '@tanstack/react-query/build/lib/QueryErrorResetBoundary';
+import { QueryErrorResetBoundaryValue } from '@tanstack/react-query/build/legacy/QueryErrorResetBoundary';
 import * as React from 'react';
 
 const ensurePreventErrorBoundaryRetry = <TQueryFnData, TError, TData, TQueryData, TQueryKey extends QueryKey>(
-    options: DefaultedQueryObserverOptions<TQueryFnData, TError, TData, TQueryData, TQueryKey>,
-    errorResetBoundary: QueryErrorResetBoundaryValue,
+  options: DefaultedQueryObserverOptions<TQueryFnData, TError, TData, TQueryData, TQueryKey>,
+  errorResetBoundary: QueryErrorResetBoundaryValue,
 ) => {
-    if (options.suspense || options.useErrorBoundary) {
-        // Prevent retrying failed query if the error boundary has not been reset yet
-        if (!errorResetBoundary.isReset()) {
-            options.retryOnMount = false;
-        }
+  if (options.suspense || options.throwOnError) {
+    // Prevent retrying failed query if the error boundary has not been reset yet
+    if (!errorResetBoundary.isReset()) {
+      options.retryOnMount = false;
     }
+  }
 };
 
 const useClearResetErrorBoundary = (errorResetBoundary: QueryErrorResetBoundaryValue) => {
-    React.useEffect(() => {
-        errorResetBoundary.clearReset();
-    }, [errorResetBoundary]);
+  React.useEffect(() => {
+    errorResetBoundary.clearReset();
+  }, [errorResetBoundary]);
 };
 
 function shouldThrowError<T extends (...args: any[]) => boolean>(
-    _useErrorBoundary: boolean | T | undefined,
-    params: Parameters<T>,
+  _throwOnError: boolean | T | undefined,
+  params: Parameters<T>,
 ): boolean {
-    // Allow useErrorBoundary function to override throwing behavior on a per-error basis
-    if (typeof _useErrorBoundary === 'function') {
-        return _useErrorBoundary(...params);
-    }
+  // Allow _throwOnError function to override throwing behavior on a per-error basis
+  if (typeof _throwOnError === 'function') {
+    return _throwOnError(...params);
+  }
 
-    return !!_useErrorBoundary;
+  return !!_throwOnError;
 }
 
 const getHasError = <TData, TError, TQueryFnData, TQueryData, TQueryKey extends QueryKey>({
-    result,
-    errorResetBoundary,
-    useErrorBoundary,
-    query,
+  result,
+  errorResetBoundary,
+  throwOnError,
+  query,
 }: {
-    result: QueryObserverResult<TData, TError>;
-    errorResetBoundary: QueryErrorResetBoundaryValue;
-    useErrorBoundary: UseErrorBoundary<TQueryFnData, TError, TQueryData, TQueryKey>;
-    query: Query<TQueryFnData, TError, TQueryData, TQueryKey>;
+  result: QueryObserverResult<TData, TError>;
+  errorResetBoundary: QueryErrorResetBoundaryValue;
+  throwOnError: ThrowOnError<TQueryFnData, TError, TQueryData, TQueryKey>;
+  query: Query<TQueryFnData, TError, TQueryData, TQueryKey>;
 }) => {
-    return (
-        result.isError &&
-        !errorResetBoundary.isReset() &&
-        !result.isFetching &&
-        shouldThrowError(useErrorBoundary, [result.error, query])
-    );
+  return (
+    result.isError &&
+    !errorResetBoundary.isReset() &&
+    !result.isFetching &&
+    shouldThrowError(throwOnError, [result.error, query])
+  );
 };
 
 export function useObservableQuery<
-    TQueryFnData,
-    TError,
-    TData = TQueryFnData,
-    TQueryData = TQueryFnData,
-    TQueryKey extends QueryKey = QueryKey,
-    TContext = unknown,
+  TQueryFnData,
+  TError,
+  TData = TQueryFnData,
+  TQueryData = TQueryFnData,
+  TQueryKey extends QueryKey = QueryKey,
+  TContext = unknown,
 >(
-    options: UseBaseQueryOptions<TQueryFnData, TError, TData, TQueryData, TQueryKey> & { queryClient?: QueryClient },
-    mutationOptions?: UseMutationOptions<TData, TError, void, TContext>,
+  options: UseBaseQueryOptions<TQueryFnData, TError, TData, TQueryData, TQueryKey> & { queryClient?: QueryClient },
+  mutationOptions?: UseMutationOptions<TData, TError, void, TContext>,
 ): ObservableObject<UseBaseQueryResult<TData, TError>> {
-    const Observer = QueryObserver;
-    const queryClient = options?.queryClient || useQueryClient({ context: options.context });
-    const isRestoring = useIsRestoring();
-    const errorResetBoundary = useQueryErrorResetBoundary();
-    const defaultedOptions = queryClient.defaultQueryOptions(options);
+  const Observer = QueryObserver;
+  const queryClient = options?.queryClient || useQueryClient(new QueryClient());
+  const isRestoring = useIsRestoring();
+  const errorResetBoundary = useQueryErrorResetBoundary();
+  const defaultedOptions = queryClient.defaultQueryOptions(options);
 
-    // Make sure results are optimistically set in fetching state before subscribing or updating options
-    defaultedOptions._optimisticResults = isRestoring ? 'isRestoring' : 'optimistic';
+  // Make sure results are optimistically set in fetching state before subscribing or updating options
+  defaultedOptions._optimisticResults = isRestoring ? 'isRestoring' : 'optimistic';
 
-    // Include callbacks in batch renders
-    if (defaultedOptions.onError) {
-        defaultedOptions.onError = notifyManager.batchCalls(defaultedOptions.onError);
+  if (defaultedOptions.suspense) {
+    // Always set stale time when using suspense to prevent
+    // fetching again when directly mounting after suspending
+    if (typeof defaultedOptions.staleTime !== 'number') {
+      defaultedOptions.staleTime = 1000;
     }
+  }
 
-    if (defaultedOptions.onSuccess) {
-        defaultedOptions.onSuccess = notifyManager.batchCalls(defaultedOptions.onSuccess);
-    }
+  ensurePreventErrorBoundaryRetry(defaultedOptions, errorResetBoundary);
 
-    if (defaultedOptions.onSettled) {
-        defaultedOptions.onSettled = notifyManager.batchCalls(defaultedOptions.onSettled);
-    }
+  useClearResetErrorBoundary(errorResetBoundary);
 
-    if (defaultedOptions.suspense) {
-        // Always set stale time when using suspense to prevent
-        // fetching again when directly mounting after suspending
-        if (typeof defaultedOptions.staleTime !== 'number') {
-            defaultedOptions.staleTime = 1000;
-        }
-    }
+  const [observer] = React.useState(
+    () => new Observer<TQueryFnData, TError, TData, TQueryData, TQueryKey>(queryClient, defaultedOptions),
+  );
 
-    ensurePreventErrorBoundaryRetry(defaultedOptions, errorResetBoundary);
+  const result = observer.getOptimisticResult(defaultedOptions);
 
-    useClearResetErrorBoundary(errorResetBoundary);
+  // useSyncExternalStore was here in useBaseQuery but is removed for Legend-State.
 
-    const [observer] = React.useState(
-        () => new Observer<TQueryFnData, TError, TData, TQueryData, TQueryKey>(queryClient, defaultedOptions),
-    );
+  React.useEffect(() => {
+    // Do not notify on updates because of changes in the options because
+    // these changes should already be reflected in the optimistic result.
+    observer.setOptions(defaultedOptions, { listeners: false });
+  }, [defaultedOptions, observer]);
 
-    const result = observer.getOptimisticResult(defaultedOptions);
+  // Handle suspense
+  if (defaultedOptions.suspense && result.isLoading && result.isFetching && !isRestoring) {
+    throw observer
+      .fetchOptimistic(defaultedOptions)
+      .then(() => {
+        // defaultedOptions.onSuccess?.(data as TData);
+        // defaultedOptions.onSettled?.(data, null);
+      })
+      .catch(() => {
+        errorResetBoundary.clearReset();
+        // defaultedOptions.onError?.(error);
+        // defaultedOptions.onSettled?.(undefined, error);
+      });
+  }
 
-    // useSyncExternalStore was here in useBaseQuery but is removed for Legend-State.
+  // Handle error boundary
+  if (
+    getHasError({
+      result,
+      errorResetBoundary,
+      throwOnError: defaultedOptions.throwOnError,
+      query: observer.getCurrentQuery(),
+    })
+  ) {
+    throw result.error;
+  }
 
-    React.useEffect(() => {
-        // Do not notify on updates because of changes in the options because
-        // these changes should already be reflected in the optimistic result.
-        observer.setOptions(defaultedOptions, { listeners: false });
-    }, [defaultedOptions, observer]);
+  // Legend-State changes from here down
+  let mutator: MutationObserver<TData, TError, void, TContext>;
+  if (mutationOptions) {
+    [mutator] = React.useState(() => new MutationObserver(queryClient, mutationOptions));
+  }
 
-    // Handle suspense
-    if (defaultedOptions.suspense && result.isLoading && result.isFetching && !isRestoring) {
-        throw observer
-            .fetchOptimistic(defaultedOptions)
-            .then(({ data }) => {
-                defaultedOptions.onSuccess?.(data as TData);
-                defaultedOptions.onSettled?.(data, null);
-            })
-            .catch((error) => {
-                errorResetBoundary.clearReset();
-                defaultedOptions.onError?.(error);
-                defaultedOptions.onSettled?.(undefined, error);
-            });
-    }
+  const [obs] = React.useState<ObservableObject<UseBaseQueryResult<TData, TError>>>(() => {
+    const obs = observable<any>(observer.getCurrentResult());
 
-    // Handle error boundary
-    if (
-        getHasError({
-            result,
-            errorResetBoundary,
-            useErrorBoundary: defaultedOptions.useErrorBoundary,
-            query: observer.getCurrentQuery(),
-        })
-    ) {
-        throw result.error;
-    }
+    let isSetting = false;
 
-    // Legend-State changes from here down
-    let mutator: MutationObserver<TData, TError, void, TContext>;
+    // If there is a mutator watch for changes as long as they don't come from the the query observer
     if (mutationOptions) {
-        [mutator] = React.useState(() => new MutationObserver(queryClient, mutationOptions));
+      observe(() => {
+        const data = obs.data.get();
+        // Don't want to call mutate if there's no data or this coming from the query changing
+        if (data && !isSetting) {
+          mutator.mutate(data);
+        }
+      });
     }
 
-    const [obs] = React.useState<ObservableObject<UseBaseQueryResult<TData, TError>>>(() => {
-        const obs = observable<any>(observer.getCurrentResult());
+    // Note: Don't need to worry about unsubscribing because the query observer itself
+    // is scoped to this component
+    observer.subscribe((result) => {
+      isSetting = true;
 
-        let isSetting = false;
-
-        // If there is a mutator watch for changes as long as they don't come from the the query observer
-        if (mutationOptions) {
-            observe(() => {
-                const data = obs.data.get();
-                // Don't want to call mutate if there's no data or this coming from the query changing
-                if (data && !isSetting) {
-                    mutator.mutate(data);
-                }
-            });
-        }
-
-        // Note: Don't need to worry about unsubscribing because the query observer itself
-        // is scoped to this component
-        observer.subscribe((result) => {
-            isSetting = true;
-
-            try {
-                // Update the observable with the latest value
-                obs.set(result);
-            } finally {
-                // If set causes a crash for some reason we still need to reset isSetting
-                isSetting = false;
-            }
-        });
-
-        return obs as unknown as ObservableObject<UseBaseQueryResult<TData, TError>>;
+      try {
+        // Update the observable with the latest value
+        obs.set(result);
+      } finally {
+        // If set causes a crash for some reason we still need to reset isSetting
+        isSetting = false;
+      }
     });
 
-    // Return the observable
-    return obs;
+    return obs as unknown as ObservableObject<UseBaseQueryResult<TData, TError>>;
+  });
+
+  // Return the observable
+  return obs;
 }
